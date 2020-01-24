247810fd41f5113486fd3b8b55b5197442c88a47
glasista <ruszala.rafal@gmail.com>
2020-01-24 17:11:54 +0100

    Adding model files

---
+++ Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeFirst.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public decimal Total { get; set; }
+        public int CustomerId { get; set; }
+        public virtual Customer Customer { get; set; }
+        public ICollection<OrderProduct> OrderProducts { get; set; }
+    }
+}
